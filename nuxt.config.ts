6233686a01f7e279cf1eb9ea5020678a924030c9
unknown <valera55555nazarchuk@gmail.com>
2023-12-21 17:53:52 +0200

    CHANGE | change favicon icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: "Base Nuxt Setup",
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
       // lang: "en",
       meta: [
         {
