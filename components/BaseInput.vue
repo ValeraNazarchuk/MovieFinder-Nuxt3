85f52b9913b1a8505281943c3ce96af6befc9fc5
unknown <valera55555nazarchuk@gmail.com>
2023-12-21 17:54:50 +0200

    FEATURE | added base input

---
+++ components/BaseInput.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { defineProps, defineEmits, toRefs } from "vue";
+
+const emits = defineEmits(["update:modelValue"]);
+
+const props = defineProps({
+  modelValue: {
+    type: [String, Number],
+    required: true,
+  },
+});
+const { modelValue } = toRefs(props);
+</script>
+
+<template>
+  <el-input
+    class="field-input"
+    :model-value="modelValue"
+    @input="emits('update:modelValue', $event)"
+  />
+</template>
+
+<style lang="scss" scoped>
+.field-input {
+  color: $fs-white;
+  &::placeholder {
+    color: $fs-white;
+  }
+}
+</style>
