7aa6c65a7417399964d3d45ee35168ca5d5c068e
unknown <valera55555nazarchuk@gmail.com>
2023-12-21 17:54:40 +0200

    FEATURE | added base button

---
+++ components/BaseButton.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { defineProps, toRefs } from "vue";
+const emits = defineEmits(["onClick"]);
+
+const props = defineProps({
+  type: {
+    type: String,
+    required: true,
+  },
+  disabled: {
+    type: Boolean,
+    required: false,
+  },
+});
+
+const { type } = toRefs(props);
+
+const handleClick = () => {
+  emits("onClick");
+};
+</script>
+
+<template>
+  <el-button :disabled="disabled" :type="type" @click="handleClick"
+    ><slot
+  /></el-button>
+</template>
+
+<style lang="scss" scoped></style>
