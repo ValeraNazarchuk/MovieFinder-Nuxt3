c2c6275d57168c0b47e0e74fe1ada4ac819d0a56
unknown <valera55555nazarchuk@gmail.com>
2023-12-21 15:15:39 +0200

    FEATURE | added url and key to .env, configured Eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,17 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  extends: ["@nuxt/eslint-config", "@vue/eslint-config-prettier"],
+  extends: [
+    "@nuxt/eslint-config",
+    "@vue/eslint-config-prettier",
+    "plugin:prettier/recommended",
+  ],
   parserOptions: {
     ecmaVersion: "latest",
   },
+  plugins: ["prettier"],
   rules: {
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
     "vue/multi-word-component-names": 0,
     "vue/no-multiple-template-root": "off",
     "vue/v-on-event-hyphenation": 0,
