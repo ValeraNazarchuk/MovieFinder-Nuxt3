2f3af31c9abe0f7be66e89d648c82b105068d72f
unknown <valera55555nazarchuk@gmail.com>
2023-12-21 17:56:49 +0200

    FEATURE | done page home and defauld layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="default">
-    <div class="page">
-      <slot />
-    </div>
+    <TheHeader />
+    <main class="main">
+      <div class="container">
+        <slot />
+      </div>
+    </main>
   </div>
 </template>
 <script></script>
 <style lang="scss" scoped>
 .default {
   position: relative;
+}
+
+.main {
+  background-color: #484747;
+  padding: 20px 0;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="home">Home page</div>
+  <div class="home">
+    <h1 class="home__title">Search movie:</h1>
+    <el-form
+      ref="ruleFormRef"
+      class="home__form"
+      :model="ruleForm"
+      :rules="rules"
+      label-position="top"
+      status-icon
+      @submit.prevent
+      @keyup.enter="searchClick(ruleFormRef)"
+    >
+      <el-form-item label="Search Movie" prop="movieValue">
+        <BaseInput v-model="ruleForm.movieValue" />
+      </el-form-item>
+      <el-form-item>
+        <BaseButton :type="'primary'" @onClick="searchClick(ruleFormRef)">
+          Search
+        </BaseButton>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 <script setup>
 // core
@@ -44,4 +65,47 @@ useHead({
     },
   ],
 });
+
+//____ my code
+const router = useRouter();
+
+const ruleFormRef = ref();
+const rules = reactive({
+  movieValue: [
+    { required: true, message: "Please input name movie", trigger: "blur" },
+    { min: 3, message: "The length should be from 3", trigger: "blur" },
+  ],
+});
+const ruleForm = reactive({
+  movieValue: "",
+});
+
+const searchClick = async (formEl) => {
+  if (!formEl) return;
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      router.push(`/movies?search=${ruleForm.movieValue}&page=1`);
+    } else {
+      alert("error submit!", fields);
+    }
+  });
+};
 </script>
+
+<style lang="scss" scoped>
+.home {
+  &__form {
+    margin: 0 auto;
+    max-width: 500px;
+    display: flex;
+    flex-direction: column;
+  }
+  &__title {
+    color: $fs-white;
+    text-align: center;
+    font-size: 30px;
+    line-height: 42px;
+    margin-bottom: 10px;
+  }
+}
+</style>
