e345e2adb4fcc824a33b9c7837462ca243809e53
unknown <valera55555nazarchuk@gmail.com>
2023-12-21 17:55:33 +0200

    FEATURE | added page form and component for form UploadFile

---
+++ pages/form.vue
@@ -0,0 +1,128 @@
+<script setup>
+import { ref, reactive } from "vue";
+
+const showUpload = ref(true);
+
+const data = reactive({
+  title: "",
+  poster: null,
+  director: "",
+  year: "",
+});
+
+const validateYear = (rule, value, callback) => {
+  const valueLength = value.toString().split("").length;
+  if (!value) {
+    return callback(new Error("Please input the Year"));
+  }
+  if (!Number.isInteger(value)) {
+    callback(new Error("Year must be a number"));
+  } else if (valueLength < 4) {
+    callback(new Error("The length should be from 4"));
+  } else {
+    callback();
+  }
+};
+
+const ruleFormRef = ref();
+const rules = reactive({
+  title: [
+    {
+      required: true,
+      message: "Please input title movie",
+      trigger: "blur",
+    },
+    { min: 3, message: "The length should be from 3", trigger: "blur" },
+  ],
+  year: [
+    { required: true, message: "Please input year movie", trigger: "blur" },
+    { validator: validateYear, trigger: "blur" },
+  ],
+  director: [
+    {
+      required: true,
+      message: "Please input movie director",
+      trigger: "blur",
+    },
+    { min: 3, message: "The length should be from 3", trigger: "blur" },
+  ],
+  poster: [
+    { required: true, message: "Please insert poster", trigger: "change" },
+  ],
+});
+
+const sendForm = async (formEl) => {
+  if (!formEl) return;
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      console.log(`
+        Title: ${data.title}
+        Year: ${data.year}
+        Director: ${data.director}
+        Poster: ${data.poster}
+        `);
+
+      data.title = "";
+      data.year = "";
+      data.director = "";
+      data.poster = null;
+
+      showUpload.value = false;
+    } else {
+      console.log("error submit!", fields);
+    }
+  });
+};
+
+const handleImageUploaded = (url) => {
+  data.poster = url;
+};
+</script>
+
+<template>
+  <el-form
+    ref="ruleFormRef"
+    :model="data"
+    :rules="rules"
+    class="form"
+    label-position="top"
+    label-width="120px"
+    status-icon
+    @submit.prevent
+  >
+    <h3 class="form__title">Sent movie</h3>
+    <el-form-item class="form__item" label="Enter Title" prop="title">
+      <BaseInput v-model="data.title" />
+    </el-form-item>
+    <el-form-item class="form__item" label="Enter Year" prop="year">
+      <BaseInput v-model.number="data.year" />
+    </el-form-item>
+    <el-form-item class="form__item" label="Enter Director" prop="director">
+      <BaseInput v-model="data.director" />
+    </el-form-item>
+    <el-form-item class="form__item" label="Enter Poster" prop="poster">
+      <FormUploadFile
+        :show-upload="showUpload"
+        @image-uploaded="handleImageUploaded"
+      />
+    </el-form-item>
+    <el-form-item>
+      <BaseButton :type="'primary'" @click="sendForm(ruleFormRef)">
+        Send
+      </BaseButton>
+    </el-form-item>
+  </el-form>
+</template>
+
+<style lang="scss" scoped>
+.form {
+  margin: 0 auto;
+  max-width: 500px;
+  &__title {
+    color: $fs-white;
+    text-align: center;
+    font-size: 32px;
+    line-height: 46px;
+  }
+}
+</style>
