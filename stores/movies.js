62d6fbef1462e2d0882e1235b38627d52b9c3f70
unknown <valera55555nazarchuk@gmail.com>
2023-12-25 00:29:21 +0200

    FEATURE | Configured a request to the server via useFetch

---
+++ stores/movies.js
@@ -0,0 +1,41 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import { api } from "@/services/api";
+
+export const useMoviesStore = defineStore("moviesStore", () => {
+  const loading = ref(false);
+  const movies = ref([]);
+  const movie = ref({});
+
+  const getListMovies = async (movie, page) => {
+    loading.value = true;
+    try {
+      const response = await api(`?s=${movie}&page=${page}`);
+      movies.value = response.Search;
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  const getDetailedMovie = async (movieId) => {
+    loading.value = true;
+    try {
+      const response = await api(`?i=${movieId}`);
+      movie.value = response;
+    } catch (error) {
+      console.error("Error fetching movie:", error);
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  return {
+    loading,
+    movies,
+    movie,
+    getListMovies,
+    getDetailedMovie,
+  };
+});
