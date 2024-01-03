19f02fcf8060f3a2d055652378f26d9da6f1b11c
unknown <valera55555nazarchuk@gmail.com>
2024-01-03 11:17:47 +0200

    CHANGE| replaced localStorage by useCookie | FavoriteMovies.js

--- stores/FavoriteMovies.js
+++ stores/FavoriteMovies.js
@@ -1,30 +1,26 @@
 import { defineStore } from "pinia";
-import { ref, watch } from "vue";
+import { ref } from "vue";
+import { useCookie } from "nuxt/app";
 
 export const useFavoriteMoviesStore = defineStore("favoriteMoviesStore", () => {
   const loading = ref(false);
-  const movies = ref([]);
-
-  const moviesOnLocalStorage = localStorage.getItem("movies");
-  if (moviesOnLocalStorage) {
-    movies.value = JSON.parse(moviesOnLocalStorage)._value;
-  }
+  const movies = useCookie("movies", {
+    default: () => [],
+  });
 
   const addMovie = (movie) => {
-    movies.value.unshift(movie);
+    const newObjectMovie = {
+      Title: movie.Title,
+      Year: movie.Year,
+      imdbID: movie.imdbID,
+      Poster: movie.Poster,
+    };
+    movies.value.unshift(newObjectMovie);
   };
 
   const deleteMovie = (movieId) => {
     movies.value = movies.value.filter((el) => movieId !== el.imdbID);
   };
-
-  watch(
-    () => movies,
-    (state) => {
-      localStorage.setItem("movies", JSON.stringify(state));
-    },
-    { deep: true }
-  );
 
   return {
     loading,
