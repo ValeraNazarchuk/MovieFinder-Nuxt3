5785a7a921562c7c5b83f7bd49ccbb68bfbcb2dc
unknown <valera55555nazarchuk@gmail.com>
2024-01-03 11:19:16 +0200

    FIX| fixed 500 error, moved useRuntimeConfig to a function | api.js

--- services/api.js
+++ services/api.js
@@ -1,11 +1,12 @@
 // core
 import { useFetch } from "@vueuse/core";
 import { useRuntimeConfig } from "nuxt/app";
-const config = useRuntimeConfig();
-const baseURL = config.public.baseURL;
-const apiKey = config.public.apiKey;
 
 export const api = async (params) => {
+  const config = useRuntimeConfig();
+  const baseURL = config.public.baseURL;
+  const apiKey = config.public.apiKey;
   const { data, error } = await useFetch(`${baseURL}${params}&${apiKey}`);
+
   return { data, error };
 };
